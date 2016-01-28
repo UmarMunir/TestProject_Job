a718bdc6cf5d5dbdcd188763c162219ee4c6bc87
umarmunir <umar.munir@logicpowered.net>
2016-01-28 19:00:35 +0500

    test commit

---
+++ WSCApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WSCApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            
+            var sitecount = int.Parse(ConfigurationManager.AppSettings["NoOfSites"]);
+            var waitTime = int.Parse(ConfigurationManager.AppSettings["WaitTime"]);
+
+            var sitename = "Site";
+            var siteurl = "";
+            for(int i=0; i< sitecount; i++ )
+            {
+                sitename = sitename+i.ToString();
+                siteurl = ConfigurationManager.AppSettings[sitename];
+
+                var request = client.GetAsync(new Uri(siteurl));
+                Trace.TraceInformation("{0}: {1}", DateTime.Now, request.Result.StatusCode);
+                sitename = "Site";
+            }
+
+
+        }
+
+      
+    }
+}
